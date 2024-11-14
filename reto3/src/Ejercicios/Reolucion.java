216df07e0c27ee117576177f0e910e86089c679a
Yowii10 <yoeliranzo@gmail.com>
2024-11-14 09:44:29 +0100

    añadido ejercicio 2 a MAIN

--- reto3/src/Ejercicios/Reolucion.java
+++ reto3/src/Ejercicios/Reolucion.java
@@ -1,10 +1,32 @@
 package Ejercicios;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import util.Funciones;
+
 public class Reolucion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		LocalDate fecha1 = Funciones.dimeFecha("Introduce una fecha dd/MM/yyyy", sc);
+		LocalDate fecha2;
+		
+		do {
+			
+		fecha2 = Funciones.dimeFecha("Introduce una segunda fecha dd/MM/yyyy", sc);
+		
+		}while (!fecha1.isBefore(fecha2));
+		
+		
+		
+		System.out.println(Ejercicio2.calcDias(fecha1, fecha2));
+		
+		
+		
+		
+		
+		}
 
 	}
 
-}
